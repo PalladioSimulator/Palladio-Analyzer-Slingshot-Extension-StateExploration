a659f1ff8a706a69889855c8c6d9099eddeb5f2c
Jonas Edlhuber <jonas.edlhuber@uni-ulm.de>
2023-12-11 15:56:01 +0100

    Fix: StateGraphConverter

--- org.palladiosimulator.analyzer.slingshot.planner/src/org/palladiosimulator/analyzer/slingshot/planner/runner/StateGraphConverter.java
+++ org.palladiosimulator.analyzer.slingshot.planner/src/org/palladiosimulator/analyzer/slingshot/planner/runner/StateGraphConverter.java
@@ -23,66 +23,73 @@ import org.palladiosimulator.analyzer.slingshot.stateexploration.change.api.Reco
 import org.palladiosimulator.monitorrepository.Monitor;
 import org.palladiosimulator.servicelevelobjective.ServiceLevelObjective;
 import org.palladiosimulator.spd.SPD;
+import org.palladiosimulator.spd.ScalingPolicy;
 
 public class StateGraphConverter {
 	public static StateGraph convert(RawStateGraph graph) {
-		Map<String, StateGraphNode> newStates = new HashMap<String, StateGraphNode>();
+		StateGraph newGraph = new StateGraph(convertState(graph.getRoot()));
 
-		for (RawModelState state : graph.getStates()) {
-			List<SLO> slos = new ArrayList<SLO>();
-			List<MeasurementSet> measuremnets = new ArrayList<MeasurementSet>();
-			List<Transition> transitions = new ArrayList<Transition>();
-			
-			/**
-			 * The following lines are needed of the Palladio System to load the Monitors. 
-			 * This is a workaround because otherwise the reading the monitor of the SLO MeasurmentDescription for the Measuring Point would be null.
-			 */
-			System.out.println("Monitors:");
-			for (Monitor monitor : state.getArchitecureConfiguration().getMonitorRepository().getMonitors()) {
-				//System.out.println(monitor.getEntityName());
-			}
-			
-			if (state.getArchitecureConfiguration() != null && state.getArchitecureConfiguration().getSLOs() != null) {
-				slos = new ArrayList<SLO>();
-				
-				for (ServiceLevelObjective slo : state.getArchitecureConfiguration().getSLOs().getServicelevelobjectives()) {
-					slos.add(PalladioSimulationsVisitor.visitServiceLevelObjective(slo));
-				}
-			}
-			
-			// getting all measurements from the RawState and add them to the newly created state
-			if (state.getMeasurements() != null) {
-				measuremnets = PalladioSimulationsVisitor.visitExperiementSetting(state.getMeasurements());
-			}
-
-			// for every transition a path is written to the file
-			for (RawTransition transition : state.getOutTransitions()) {
-				Reason reason = null;
-				org.palladiosimulator.analyzer.slingshot.planner.data.Change change = null;
-				
-				if (!transition.getChange().isPresent()) // no change in PCM instance -> measurement change
-					reason = Reason.IntervalChange;
-				else if (transition.getChange().get() instanceof EnvironmentChange) // environment change
-					reason = Reason.EnviromentalChange;
-				else if (transition.getChange().get() instanceof Reconfiguration) { // reconfiguration change
-					reason = Reason.ReconfigurationChange;
-					Optional<Change> changeOrg = transition.getChange();
-					if (changeOrg.isPresent()) {
-						String scalingPolicyId = ((Reconfiguration) changeOrg.get()).getScalingPolicy().getId();
-						double timeStamp = transition.getSource().getEndTime();
-						
-						change = new ReconfigurationChange(scalingPolicyId, timeStamp);
-					}
-					
-				} else if (transition.getChange().get() instanceof ReactiveReconfiguration)  // reactive reconfiguration change
-					reason = Reason.ReactiveReconfigurationChange;
-				
-				transitions.add(new Transition(newStates.get(transition.getTarget().getId()),newStates.get(transition.getSource().getId()), reason, Optional.ofNullable(change)));
-			}
-			
-			newStates.put(state.getId(), new StateGraphNode(state.getId(), transitions, state.getStartTime(), state.getEndTime(), measuremnets, slos));
+		// set source from null to correct value
+	    for (StateGraphNode s : newGraph.states()) {
+	    	List<Transition> trans = new ArrayList<Transition>();
+	    	trans.addAll(s.outTransitions());
+	    	s.outTransitions().clear();
+	    	
+	    	for (Transition t : trans) {
+	    		s.outTransitions().add(new Transition(s, t.target(), t.reason(), t.change()));
+	    	}
+	    }
+	    
+	    return newGraph;
+	}
+	
+	private static StateGraphNode convertState(RawModelState state) {
+		List<SLO> slos = new ArrayList<SLO>();
+		List<MeasurementSet> measuremnets = new ArrayList<MeasurementSet>();
+		List<Transition> transitions = new ArrayList<Transition>();
+		
+		/**
+		 * The following lines are needed of the Palladio System to load the Monitors. 
+		 * This is a workaround because otherwise the reading the monitor of the SLO MeasurmentDescription for the Measuring Point would be null.
+		 */
+		for (Monitor monitor : state.getArchitecureConfiguration().getMonitorRepository().getMonitors()) {
+			//System.out.println(monitor.getEntityName());
 		}
 		
-		return new StateGraph(newStates.get(graph.getRoot().getId()));
+		if (state.getArchitecureConfiguration() != null && state.getArchitecureConfiguration().getSLOs() != null) {
+			slos = new ArrayList<SLO>();
+			
+			for (ServiceLevelObjective slo : state.getArchitecureConfiguration().getSLOs().getServicelevelobjectives()) {
+				slos.add(PalladioSimulationsVisitor.visitServiceLevelObjective(slo));
+			}
+		}
+		
+		// getting all measurements from the RawState and add them to the newly created state
+		if (state.getMeasurements() != null) {
+			measuremnets = PalladioSimulationsVisitor.visitExperiementSetting(state.getMeasurements());
+		}
+
+		// for every transition a path is written to the file
+		for (RawTransition transition : state.getOutTransitions()) {
+			Reason reason = null;
+			org.palladiosimulator.analyzer.slingshot.planner.data.Change change = null;
+			
+			if (!transition.getChange().isPresent()) // no change in PCM instance -> measurement change
+				reason = Reason.IntervalChange;
+			else if (transition.getChange().get() instanceof EnvironmentChange) // environment change
+				reason = Reason.EnviromentalChange;
+			else if (transition.getChange().get() instanceof Reconfiguration) { // reconfiguration change
+				reason = Reason.ReconfigurationChange;
+				Optional<Change> changeOrg = transition.getChange();
+				if (changeOrg.isPresent())					
+					change = new ReconfigurationChange(((Reconfiguration) changeOrg.get()).getScalingPolicy(), transition.getSource().getEndTime());
+			} else if (transition.getChange().get() instanceof ReactiveReconfiguration)  // reactive reconfiguration change
+				reason = Reason.ReactiveReconfigurationChange;
+			
+			// source is null to prevent infinity loop
+			transitions.add(new Transition(null, convertState(transition.getTarget()), reason, Optional.ofNullable(change)));
+		}
+		
+		return new StateGraphNode(state.getId(), transitions, state.getStartTime(), state.getEndTime(), measuremnets, slos);
 	}
 }
