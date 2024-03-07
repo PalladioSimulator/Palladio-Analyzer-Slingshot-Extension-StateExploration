0a5f5e8de11467ed2eadbbb9ccb3d002bce0ab87
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-03-07 11:56:19 +0100

    delete unintentionally committed files

--- fooo/the/loop/trial/events/TrialSystemEvent.java
+++
@@ -1,7 +0,0 @@
-package close.the.loop.trial.events;
-
-import org.palladiosimulator.analyzer.slingshot.common.events.AbstractSystemEvent;
-
-public class TrialSystemEvent extends AbstractSystemEvent {
-
-}
