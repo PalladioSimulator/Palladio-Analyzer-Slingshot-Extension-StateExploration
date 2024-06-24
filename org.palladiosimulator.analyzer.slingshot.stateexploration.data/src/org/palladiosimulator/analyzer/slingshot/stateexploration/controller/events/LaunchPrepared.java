7cd6cde9f178a818b21928e88acdcf9e45b6ed19
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-06-24 15:56:32 +0200

    renames, removes and other beautification.

--- org.palladiosimulator.analyzer.slingshot.stateexploration.data/src/org/palladiosimulator/analyzer/slingshot/stateexploration/controller/events/LaunchPrepared.java
+++ org.palladiosimulator.analyzer.slingshot.stateexploration.data/src/org/palladiosimulator/analyzer/slingshot/stateexploration/controller/events/LaunchPrepared.java
@@ -5,50 +5,34 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.palladiosimulator.analyzer.slingshot.common.events.AbstractSystemEvent;
-import org.palladiosimulator.analyzer.slingshot.core.api.SimulationDriver;
-import org.palladiosimulator.analyzer.slingshot.stateexploration.api.GraphExplorer;
 
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 /**
- * Event to announce a new {@link GraphExplorer} to the
+ * Event to forward information about the launch to the
  * {@code ExplorerControllerSystemBehaviour}.
- *
- * Could (should) probably be done using the injection mechanism, instead of an
- * event. [S3]
  *
  * @author Sarah Stieß
  *
  */
-public class ExplorerCreated extends AbstractSystemEvent implements ExplorationControllerEvent {
+public class LaunchPrepared extends AbstractSystemEvent implements ExplorationControllerEvent {
 
 	private final List<String> pcmModelFiles;
-	private final SimulationDriver driver;
 	private final Map<String, Object> launchConfigurationParams;
 	private final IProgressMonitor monitor;
 	private final MDSDBlackboard blackboard;
 
-
-
-	public ExplorerCreated(final List<String> pcmModelFiles, final SimulationDriver driver,
+	public LaunchPrepared(final List<String> pcmModelFiles,
 			final Map<String, Object> launchConfigurationParams, final IProgressMonitor monitor,
-			final MDSDBlackboard blackboard)
-	{
+			final MDSDBlackboard blackboard) {
 		this.pcmModelFiles = pcmModelFiles;
-		this.driver = driver;
 		this.launchConfigurationParams = launchConfigurationParams;
 		this.monitor = monitor;
 		this.blackboard = blackboard;
 	}
 
-
-
 	public List<String> getPcmModelFiles() {
 		return pcmModelFiles;
-	}
-
-	public SimulationDriver getDriver() {
-		return driver;
 	}
 
 	public Map<String, Object> getLaunchConfigurationParams() {
