6ef619578867ac9dc435a06c25e456c1cbfaef1a
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-10-18 08:42:54 +0200

    replace utility calculation.

--- org.palladiosimulator.analyzer.slingshot.injection.utility/src/org/palladiosimulator/analyzer/slingshot/injection/utility/converter/data/Utility.java
+++ org.palladiosimulator.analyzer.slingshot.injection.utility/src/org/palladiosimulator/analyzer/slingshot/injection/utility/converter/data/Utility.java
@@ -4,45 +4,56 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Utility {
-	private double totalUtility;
-	private List<UtilityData> data;
+    private double totalUtility;
+    private List<UtilityData> data;
 
+    public Utility() {
+        super();
+        this.totalUtility = 0;
+        this.data = new LinkedList<>();
+    }
 
-	public Utility() {
-		super();
-		this.totalUtility = 0;
-		this.data = new LinkedList<>();
-	}
+    public double getTotalUtilty() {
+        return totalUtility;
+    }
 
-	public double getTotalUtilty() {
-		return totalUtility;
-	}
+    public void setTotalUtilty(final double totalUtilty) {
+        this.totalUtility = totalUtilty;
+    }
 
-	public void setTotalUtilty(final double totalUtilty) {
-		this.totalUtility = totalUtilty;
-	}
+    public List<UtilityData> getData() {
+        return data;
+    }
 
-	public List<UtilityData> getData() {
-		return data;
-	}
+    public void setData(final List<UtilityData> data) {
+        this.data = data;
+    }
 
-	public void setData(final List<UtilityData> data) {
-		this.data = data;
-	}
+    public void addDataInstance(final String id, final double d, final UtilityType type) {
+        data.add(new UtilityData(id, d, type));
+    }
 
-	public void addDataInstance(final String id, final double d, final UtilityType type) {
-		data.add(new UtilityData(id, d, type));
-	}
-
-	public void calculateTotalUtility() {
-		this.totalUtility = data.stream().mapToDouble(x -> x.utility()).sum();
-	}
-
+    public void calculateTotalUtility() {
+        final var slo_util = data.stream()
+            .filter(x -> UtilityType.SLO.equals(x.type()))
+            .mapToDouble(x -> x.utility())
+            .sum();
+        final var costs = data.stream()
+            .filter(x -> UtilityType.COST.equals(x.type()))
+            .mapToDouble(x -> x.utility())
+            .sum();
+        this.totalUtility = slo_util / -costs;
+        if (Double.isNaN(this.totalUtility)) {
+            this.totalUtility = 0;
+        } else if (Double.isInfinite(this.totalUtility)) {
+            this.totalUtility = Double.MAX_VALUE * Math.signum(this.totalUtility);
+        }
+    }
 
     public record UtilityData(String id, double utility, UtilityType type) {
     }
 
     public enum UtilityType {
-		SLO, COST;
-	}
+        SLO, COST;
+    }
 }
