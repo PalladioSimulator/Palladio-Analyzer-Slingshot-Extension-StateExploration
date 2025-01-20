10fe8b32baafe45c489d24a5f1e1b36414063a78
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2025-01-20 15:29:14 +0100

    add hashcode and equals ro reconfiguration

--- bundles/org.palladiosimulator.analyzer.slingshot.stateexploration.data/src/org/palladiosimulator/analyzer/slingshot/stateexploration/change/api/Reconfiguration.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.stateexploration.data/src/org/palladiosimulator/analyzer/slingshot/stateexploration/change/api/Reconfiguration.java
@@ -1,8 +1,7 @@
 package org.palladiosimulator.analyzer.slingshot.stateexploration.change.api;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.data.ModelAdjustmentRequested;
 import org.palladiosimulator.spd.ScalingPolicy;
@@ -44,4 +43,28 @@ public abstract class Reconfiguration implements Change {
 				.map(e -> e.getScalingPolicy().getEntityName() + "[" + e.getScalingPolicy().getId() + "]")
 				.reduce("", (a, b) -> a + ", " + b) + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		String tmp = events.stream().map(e -> e.getScalingPolicy().getId()).reduce("", (s1,s2) -> s1+s2);
+		return Objects.hash(tmp);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reconfiguration other = (Reconfiguration) obj;
+		
+		String thisTmp = events.stream().map(e -> e.getScalingPolicy().getId()).reduce("", (s1,s2) -> s1+s2);
+		String otherTmp = other.events.stream().map(e -> e.getScalingPolicy().getId()).reduce("", (s1,s2) -> s1+s2);
+		
+		return Objects.equals(thisTmp, otherTmp);
+	}
+	
+	
 }
